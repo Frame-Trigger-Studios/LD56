d8a79166ef00de0a06d943347f52fae65ae64806
Peter Mandile <peterm1994@hotmail.com>
2024-10-05 20:51:24 +1000

    added upgrades

--- src/Train.ts
+++ src/Train.ts
@@ -1,4 +1,14 @@
-import {Component, Entity, Key, MathUtil, RenderCircle, System, Timer} from "lagom-engine";
+import {
+    CircleCollider,
+    CollisionSystem,
+    Component,
+    Entity,
+    Key,
+    MathUtil,
+    RenderCircle,
+    System,
+    Timer
+} from "lagom-engine";
 import {LD56} from "./LD56.ts";
 import {Layers} from "./Layers.ts";
 import {Upgrade} from "./upgrades/Upgrade.ts";
@@ -6,26 +16,30 @@ import {Bullet} from "./Bullet.ts";
 
 const CENTRE_DIST = 48;
 
-export class Carriage extends Entity
-{
-    constructor(readonly startAngle: number)
-    {
+export class Carriage extends Entity {
+    constructor(readonly startAngle: number) {
         const vec = MathUtil.lengthDirXY(CENTRE_DIST, startAngle);
         super("carriage", LD56.MID_X + vec.x, LD56.MID_Y + vec.y, Layers.TRAIN);
     }
 
-    onAdded()
-    {
+    onAdded() {
         super.onAdded();
 
-        this.addComponent(new RenderCircle(0, 0, 5, 0x00FF00));
+        const radius = 5;
+        this.addComponent(new RenderCircle(0, 0, radius, 0x00FF00));
         this.addComponent(new Gun());
+
         this.addComponent(new CarControllable(this.startAngle));
+        this.addComponent(
+            new CircleCollider(<CollisionSystem<any>>this.getScene().getGlobalSystem<CollisionSystem<any>>(CollisionSystem),
+                {
+                    layer: Layers.TRAIN,
+                    radius: radius
+                }));
     }
 }
 
-export class Gun extends Component
-{
+export class Gun extends Component {
     speed: number = 100;
     damage: number = 1;
     sizeMulti: number = 1;
@@ -37,29 +51,23 @@ export class Gun extends Component
     time: number = 0;
     shootTime: number = -1;
 
-    constructor(upgrades: Upgrade[] = [])
-    {
+    constructor(upgrades: Upgrade[] = []) {
         super();
     }
 
-    shoot(entity: Entity, angle: number)
-    {
+    shoot(entity: Entity, angle: number) {
         const angleVar = MathUtil.degToRad(5);
         let startAngle;
 
-        if (this.count % 2 === 0)
-        {
+        if (this.count % 2 === 0) {
             const offset = angleVar / 2.0;
             startAngle = -offset - ((this.count - 2) / 2.0) * angleVar;
-        } else
-        {
+        } else {
             startAngle = ((this.count - 1) / 2.0) * angleVar;
         }
 
-        for (let batch = 0; batch < this.batchSize; batch++)
-        {
-            for (let count = 0; count < this.count; count++)
-            {
+        for (let batch = 0; batch < this.batchSize; batch++) {
+            for (let count = 0; count < this.count; count++) {
                 entity.addComponent(new Timer(this.batchDelay * batch, null, false)).onTrigger.register((caller, data) => {
                     entity.scene.addEntity(new Bullet(entity.transform.x, entity.transform.y, angle + startAngle + (count * angleVar), {
                         damage: this.damage,
@@ -72,22 +80,17 @@ export class Gun extends Component
     }
 }
 
-export class CarControllable extends Component
-{
-    constructor(public angleRad: number)
-    {
+export class CarControllable extends Component {
+    constructor(public angleRad: number) {
         super();
     }
 }
 
-export class BulletSpawner extends System<[Gun, CarControllable]>
-{
-    update(delta: number): void
-    {
+export class BulletSpawner extends System<[Gun, CarControllable]> {
+    update(delta: number): void {
         this.runOnEntities((entity, gun, car) => {
             gun.time += delta;
-            if (gun.time > gun.shootTime)
-            {
+            if (gun.time > gun.shootTime) {
                 gun.shoot(entity, car.angleRad);
                 gun.shootTime = gun.reloadTime;
                 gun.time = 0;
@@ -99,23 +102,19 @@ export class BulletSpawner extends System<[Gun, CarControllable]>
 
 }
 
-export class CarMover extends System<[CarControllable]>
-{
-    ROT_SPEED = 1;
+export class CarMover extends System<[CarControllable]> {
+    static ROT_SPEED = 1;
 
-    update(delta: number): void
-    {
+    update(delta: number): void {
         const keyboard = this.scene.game.keyboard
         this.runOnEntities((entity, car) => {
-            if (keyboard.isKeyDown(Key.KeyA))
-            {
+            if (keyboard.isKeyDown(Key.KeyA)) {
                 // Rotate left
-                car.angleRad -= this.ROT_SPEED * delta / 1000;
+                car.angleRad -= CarMover.ROT_SPEED * delta / 1000;
             }
-            if (keyboard.isKeyDown(Key.KeyD))
-            {
+            if (keyboard.isKeyDown(Key.KeyD)) {
                 // Rotate right
-                car.angleRad += this.ROT_SPEED * delta / 1000;
+                car.angleRad += CarMover.ROT_SPEED * delta / 1000;
             }
 
             const vec = MathUtil.lengthDirXY(CENTRE_DIST, car.angleRad);
